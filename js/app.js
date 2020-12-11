f53729597b7996d215aa5a3070059050bc2e3ab1
张启航 <zqh0104@163.com>
2020-12-11 18:22:40 +0800

    部署到服务器

--- js/app.js
+++ js/app.js
@@ -7,11 +7,11 @@ window.app = {
     cookieDomain: "",                                                       // cookie 域
 
     /* 生产环境 */
-    // serverUrl: "http://api.z.mukewang.com:8088/foodie-dev-api",                      // 接口服务接口地址
+    // serverUrl: "http://139.196.156.193:8088/foodie-dev-api",                      // 接口服务接口地址
     paymentServerUrl: "http://payment.t.mukewang.com/foodie-payment",       // 支付中心服务地址
-    // shopServerUrl: "http://shop.z.mukewang.com:8080/foodie-shop/",                            // 门户网站地址
-    // centerServerUrl: "http://center.z.mukewang.com:8080/foodie-center/",                        // 用户中心地址
-    // cookieDomain: ".z.mukewang.com;",                                       // cookie 域
+    // shopServerUrl: "http://139.196.156.193:8080/foodie-shop/",                            // 门户网站地址
+    // centerServerUrl: "http://139.196.156.193:8080/foodie-center/",                        // 用户中心地址
+    // cookieDomain: "139.196.156.193;",                                     // cookie 域
 
     ctx: "/foodie-shop",
 
@@ -38,6 +38,7 @@ window.app = {
         var exp = new Date(); 
         exp.setTime(exp.getTime() + Days*24*60*60*1000);
         var cookieContent = name + "="+ encodeURIComponent (value) + ";path=/;";
+        debugger;
         if (this.cookieDomain != null && this.cookieDomain != undefined && this.cookieDomain != '') {
             cookieContent += "domain=" + this.cookieDomain;
         }
